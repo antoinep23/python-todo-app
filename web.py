fc3d2bba79723120b9702f63f87c391c3921da01
Antoine <141731510+antoinep23@users.noreply.github.com>
2024-07-14 12:00:54 +0100

    Implement the "complete" feature

--- web.py
+++ web.py
@@ -13,8 +13,13 @@ def add_todo():
 st.title('My Todo app')
 st.write('This app is to increase your productivity')
 
-for todo in todos:
-    st.checkbox(todo)
+for i, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(i)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label='', placeholder='Add new todo...',
               on_change=add_todo, key='user_input')
